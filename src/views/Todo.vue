954a8cda0c3de39461e61b7d28586255ec6a313a
textworld <text.zwb@gmail.com>
2020-08-28 08:36:38 +0800

    save

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,10 +1,38 @@
 <template>
-
+    <div>
+        <p>
+            <input type="text">
+        </p>
+        <div v-for="(v, k) in todos" :key="'todo' + v.id">
+            <template v-if="!v.isEdit">{{v.content}}</template> <input v-else /> <button @click="edit(k)">编辑</button> <button @click="del(k)">删除</button>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Todo"
+        name: "Todo",
+        data(){
+            return {
+                todos: []
+            }
+        },
+        created(){
+            for(let i = 0; i < 10; i++){
+                this.todos.push({
+                    id: i,
+                    content: 'a' + i
+                })
+            }
+        },
+        methods: {
+            del(key){
+                this.todos.splice(key, 1)
+            },
+            edit(key){
+                this.$set(this.todos[key], 'isEdit', true)
+            }
+        }
     }
 </script>
 
